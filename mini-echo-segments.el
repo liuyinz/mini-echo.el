5938135ea6ebefe12cb402e2946e2ec14ff5a51d
liuyinz <liuyinz95@gmail.com>
2023-10-13 05:38:35 +0800

    chore(init): add mini-echo.el

---
+++ mini-echo-segments.el
@@ -0,0 +1,155 @@
+;;; mini-echo-segments.el --- summary -*- lexical-binding: t -*-
+
+;; This file is not a part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;; This file is not a part of GNU Emacs.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'cl-lib)
+
+(defvar meow--indicator)
+(defvar evil-state)
+(defvar evil-visual-beginning)
+(defvar evil-visual-end)
+
+(declare-function flymake--mode-line-counter "flymake")
+
+(defcustom mini-echo-position-format "%l:%c,%p"
+  "Format used to display lin, number and percentage in mini echo."
+  :type 'string
+  :group 'mini-echo)
+
+;; faces
+(defface mini-echo-major-mode
+  '((t (:inherit bold)))
+  "Face for mini-echo segment of major mode.")
+
+(defface mini-echo-buffer-size
+  '((t (:inherit default)))
+  "Face for mini-echo segment of buffer size.")
+
+(defface mini-echo-buffer-position
+  '((t (:foreground "#FF6EB4")))
+  "Face for mini-echo segment of buffer position.")
+
+(defface mini-echo-remote-host
+  '((t (:foreground "#E27E8D")))
+  "Face for mini-echo segment of remote host.")
+
+(defface mini-echo-process
+  '((t (:foreground "#E27E8D")))
+  "Face for mini-echo segment of process.")
+
+(defface mini-echo-selection-info
+  '((t (:foreground "#EBBF83" :bold t)))
+  "Face for mini-echo segment of selection info.")
+
+(defvar mini-echo-segment-alist nil)
+
+(defmacro mini-echo-define-segment (name &rest body)
+  "Define a mini-echo segment NAME with DOCSTRING and BODY."
+  (declare (indent defun) (doc-string 2))
+  (let ((sym (intern (format "mini-echo-segment--%s" name)))
+        (docstring (if (stringp (car body))
+                       (pop body)
+                     (format "Display %s in mini-echo" name))))
+    (cond ((and (symbolp (car body))
+                (not (cdr body)))
+           `(add-to-list 'mini-echo-segment-alist
+                         (cons ,name ',(car body))))
+          (t
+           `(progn
+              (defun ,sym () ,docstring ,@body)
+              (add-to-list 'mini-echo-segment-alist (cons ,name ',sym)))))))
+
+(mini-echo-define-segment "major-mode"
+  "Display the major mode in mini-echo."
+  (when-let ((str (format-mode-line mode-name)))
+    (propertize str 'face 'mini-echo-major-mode)))
+
+(mini-echo-define-segment "buffer-position"
+  "Display the cursor position of current buffer."
+  (when-let ((str (string-replace "Bottom" "Bot"
+                                  (format-mode-line mini-echo-position-format))))
+    (propertize str 'face 'mini-echo-buffer-position)))
+
+(mini-echo-define-segment "buffer-size"
+  "Display the size of current buffer."
+  (when-let ((str (format-mode-line "%I")))
+    (propertize str 'face 'mini-echo-buffer-size)))
+
+(mini-echo-define-segment "remote-host"
+  "Display the hostname of remote buffer."
+  (when default-directory
+    (when-let ((host (file-remote-p default-directory 'host)))
+      (propertize (concat "@" host) 'face 'mini-echo-remote-host))))
+
+(mini-echo-define-segment "process"
+  "Display process info."
+  (when-let ((str (format-mode-line mode-line-process)))
+    (propertize str 'face 'mini-echo-process)))
+
+(mini-echo-define-segment "meow"
+  "Display the meow status of current buffer."
+  (when (bound-and-true-p meow-mode)
+    (string-trim meow--indicator)))
+
+(mini-echo-define-segment "flymake"
+  "Display flymake diagnostics of current buffer."
+  (when (bound-and-true-p flymake-mode)
+    (mapconcat
+     (lambda (s)
+       (let ((counter (cadr (flymake--mode-line-counter s t))))
+         (propertize (or (plist-get counter :propertize) "0")
+                     'face
+                     (plist-get counter 'face))))
+     '(:error :warning :note) "/")))
+
+(defsubst mini-echo-column (pos)
+  "Get the column of the position `POS'."
+  (save-excursion (goto-char pos)
+                  (current-column)))
+
+(mini-echo-define-segment "selection-info"
+  "Display current selection in current buffer."
+  (when (or mark-active (and (bound-and-true-p evil-local-mode)
+                             (eq evil-state 'visual)))
+    (cl-destructuring-bind (beg . end)
+        (if (and (bound-and-true-p evil-local-mode) (eq evil-state 'visual))
+            (cons evil-visual-beginning evil-visual-end)
+          (cons (region-beginning) (region-end)))
+      (let ((lines (count-lines beg (min end (point-max)))))
+        (propertize
+         (cond ((or (bound-and-true-p rectangle-mark-mode)
+                    (and (bound-and-true-p evil-visual-selection)
+                         (eq 'block evil-visual-selection)))
+                (let ((cols (abs (- (mini-echo-column end)
+                                    (mini-echo-column beg)))))
+                  (format "%dx%dB" lines cols)))
+               ((and (bound-and-true-p evil-visual-selection)
+                     (eq evil-visual-selection 'line))
+                (format "%dL" lines))
+               ((> lines 1)
+                (format "%dC,%dL" (- end beg) lines))
+               (t
+                (format "%dC" (- end beg))))
+         'face 'mini-echo-selection-info)))))
+
+(provide 'mini-echo-segments)
+;;; mini-echo-segments.el ends here
---
+++ mini-echo.el
@@ -0,0 +1,147 @@
+;;; mini-echo.el --- Show buffer status in echo area -*- lexical-binding: t -*-
+
+;; Copyright (C) 2023 liuyinz
+
+;; Author: liuyinz <liuyinz95@gmail.com>
+;; Maintainer: liuyinz <liuyinz95@gmail.com>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "27.1"))
+;; Keywords:
+;; Homepage: https://github.com/liuyinz/mini-echo.el
+
+;; This file is not a part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;; This file is not a part of GNU Emacs.
+
+;;; Commentary:
+
+;; Show buffer status in echo area instead of modeline.
+
+;;; Code:
+
+(require 'mini-echo-segments)
+
+(defgroup mini-echo nil
+  "docstring"
+  :group 'mini-echo)
+
+(defcustom mini-echo-segments
+  '("selection-info" "process" "flymake" "buffer-size" "buffer-position"
+    "major-mode" "meow")
+  ""
+  :type '(repeat symbol)
+  :group 'mini-echo)
+
+(defvar mini-echo--old-mdf nil)
+(defvar mini-echo-overlays nil)
+
+(defun mini-echo-show-divider (&optional hide)
+  "docstring"
+  ;; TODO recover values if disable the mode
+  (setq window-divider-default-places t)
+  (setq window-divider-default-bottom-width 1)
+  (setq window-divider-default-right-width 1)
+  (if (null hide)
+      (window-divider-mode 1)
+    (window-divider-mode -1)))
+
+(defun mini-echo-hide-modeline (&optional show)
+  "docstring"
+  (if (null show)
+      (progn
+        (setq mini-echo--old-mdf mode-line-format)
+        (setq-default mode-line-format nil))
+    (setq-default mode-line-format mini-echo--old-mdf)
+    (setq-default mini-echo--old-mdf nil))
+  (when (called-interactively-p 'any)
+    (redraw-display)))
+
+(defun mini-echo-init-overlays (&optional deinit)
+  "docstring"
+  (if (null deinit)
+      (dolist (buf '(" *Echo Area 0*" " *Minibuf-0*"))
+        (with-current-buffer (get-buffer-create buf)
+          (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+            (push ov mini-echo-overlays))))
+    (mapc #'delete-overlay mini-echo-overlays)
+    (setq mini-echo-overlays nil)))
+
+(defun mini-echo-set-text (text)
+  "docstring"
+  (unless (active-minibuffer-window)
+    (let* ((width (+ 2 (string-width text)))
+           (info (concat (propertize "  " 'cursor 1 'display
+                                     `(space :align-to (- right-fringe ,width)))
+                         text)))
+      (dolist (ov mini-echo-overlays)
+        (overlay-put ov 'after-string info))
+
+      ;; Display the text in Minibuf-0
+      (with-current-buffer " *Minibuf-0*"
+        (delete-region (point-min) (point-max))
+        (insert info)))))
+
+(defun mini-echo-get-frame-width ()
+  "docstring"
+  (with-selected-frame (window-frame (minibuffer-window))
+    (- (frame-width) left-margin-width right-margin-width)))
+
+(defun mini-echo-get-segment-string (segment)
+  "Return string of SEGMENT info."
+  (or (funcall (cdr (assoc segment mini-echo-segment-alist))) ""))
+
+(defun mini-echo-build-info ()
+  "docstring"
+  (condition-case nil
+      (mapconcat 'identity (seq-remove #'string-empty-p
+                                       (mapcar #'mini-echo-get-segment-string
+                                               mini-echo-segments))
+                 " ")
+    (format "error happends")))
+
+(defun mini-echo-update ()
+  "docstring"
+  (let* ((active-info (mini-echo-build-info))
+         (echo-message (substring-no-properties (or (current-message) "")))
+         (last-line (car (last (split-string echo-message "\n"))))
+         (blank-width (- (mini-echo-get-frame-width)
+                         (string-width active-info)
+                         (string-width last-line))))
+    (mini-echo-set-text (if (> blank-width 0) active-info ""))))
+
+;;;###autoload
+(define-minor-mode mini-echo-mode
+  "Minor mode to show buffer status in echo area."
+  :group 'mini-echo
+  :global t
+  :init-value nil
+  (if mini-echo-mode
+      (progn
+        (mini-echo-show-divider)
+        (mini-echo-hide-modeline)
+        (mini-echo-init-overlays)
+        ;; added overlays when switch to a new frame
+        ;; (add-function :after after-focus-change-function
+        ;;               #'mini-echo-update)
+        (run-with-timer 0 0.2 #'mini-echo-update))
+    (mini-echo-show-divider 'hide)
+    (mini-echo-hide-modeline 'show)
+    (mini-echo-init-overlays 'deinit)
+    ;; (remove-function after-focus-change-function #'mini-echo-update)
+    (cancel-function-timers #'mini-echo-update)))
+
+(provide 'mini-echo)
+;;; mini-echo.el ends here
